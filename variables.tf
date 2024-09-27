9fe10f8f9d2657be53970c1142078e0f026618e5
starvos <khchung87@gmail.com>
2024-09-27 21:29:43 +0800

    checked fmt

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "env" {
-    type = string
-    description = "set environment type"
+  type        = string
+  description = "set environment type"
 }
 
 variable "lambda_mem" {
-    type = number
-    default = 1024
-    description = "lambda memory"
+  type        = number
+  default     = 1024
+  description = "lambda memory"
 }
 
 variable "lambda_timeout" {
-    type = number
-    default = 6
-    description = "execution time in seconds before lambda function timeout"
+  type        = number
+  default     = 6
+  description = "execution time in seconds before lambda function timeout"
 }
 
 
