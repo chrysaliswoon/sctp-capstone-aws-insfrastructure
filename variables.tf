56b7a4754ce27e82cff0b7aeb346091070ce6001
starvos <khchung87@gmail.com>
2024-09-27 21:27:50 +0800

    include data type for variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,18 @@
-variable "env" {}
+variable "env" {
+    type = string
+    description = "set environment type"
+}
 
-variable "lambda_mem" {}
+variable "lambda_mem" {
+    type = number
+    default = 1024
+    description = "lambda memory"
+}
 
-variable "lambda_timeout" {}
+variable "lambda_timeout" {
+    type = number
+    default = 6
+    description = "execution time in seconds before lambda function timeout"
+}
 
 
